36793131d2009aa210a11e754eddf08bacf82c97
ttaylo201 <Thomas_Taylor2@cable.comcast.com>
2013-01-03 15:16:06 -0700

    Fixed color escape sequences

--- lib/project.sh
+++ lib/project.sh
@@ -8,17 +8,19 @@ function __rim_create_project() {
   echo -e "${green}Preparing project: ${red}$project_name${clear_color}" >&2
   mkdir -p $project_name
 
-  echo -e "${red}"
+  echo -en "${red}"
   cd $project_name
     git init
     mkdir -p "lib" "test"
 
-    echo -n "source 'http://rubygems.org'\n" >> 'Gemfile'
-    echo -n "DO SOMETHING\n" >> 'README.md'
-    echo -n "/tmp\n/log\n/.bundle\n/db/*.sqlite3" >> '.gitignore'
-    echo -n "require 'rake'\nrequire 'rake/testtask'\n\nRake::TestTask.new do |t|\n  t.pattern = 'test/**/*_test.rb'\n  t.libs << 'test'\nend\n" >> 'Rakefile'
-    echo -n "require 'minitest/unit'\nrequire 'minitest/autorun'\n\nrequire '$project_name'\n" >> 'test/test_helper.rb'
+    echo -e "source 'http://rubygems.org'\n" >> 'Gemfile'
+    echo -e "DO SOMETHING\n" >> 'README.md'
+    echo -e "/tmp\n/log\n/.bundle\n/db/*.sqlite3\n" >> '.gitignore'
+    echo -en "require 'rake'\nrequire 'rake/testtask'\n" >> 'Rakefile'
+    echo -e "Rake::TestTask.new do |t|\n  t.pattern = 'test/**/*_test.rb'\n  t.libs << 'test'\nend\n" >> 'Rakefile'
+    echo -en "require 'minitest/unit'\nrequire 'minitest/autorun'\n" >> 'test/test_helper.rb'
+    echo -e "require '$project_name'\n" >> 'test/test_helper.rb'
   cd $curr_dir
-  echo -e  "${clear_color}"
+  echo -ne  "${clear_color}"
 }
 
