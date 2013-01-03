5e94b4e3b30967c7cfb6fc4512272e61cfbb1f4b
ttaylo201 <Thomas_Taylor2@cable.comcast.com>
2013-01-03 13:05:57 -0700

    Added project creation function.

---
+++ lib/colors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+black="\e[30m"
+red="\e[31m"
+dark_red="\e[1;31m"
+green="\e[32m"
+dark_green="\e[1;32m"
+yellow="\e[33m"
+dark_yellow="\e[1;33m"
+blue="\e[34m"
+dark_blue="\e[1;34m"
+purple="\e[35m"
+dark_purple="\e[1;35m"
+cyan="\e[36m"
+dark_cyan="\e[1;36m"
+clear_color="\e[0m"
+
---
+++ lib/project.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function __rim_create_project() {
+  local project_name=$1; shift
+  local options=$@
+  local curr_dir=$(pwd)
+
+  echo -e "${green}Preparing project: ${red}$project_name${clear_color}" >&2
+  mkdir -p $project_name
+
+  echo -e "${red}"
+  cd $project_name
+    git init
+    bundle init
+    mkdir -p "lib" "test"
+
+    echo -n "DO SOMETHING\n" >> 'README.md'
+    echo -n "/tmp\n/log\n/.bundle\n/db/*.sqlite3" >> '.gitignore'
+    echo -n "require 'rake'\nrequire 'rake/testtask'\n\nRake::TestTask.new do |t|\n  t.pattern = 'test/**/*_test.rb'\n  t.libs << 'test'\nend\n" >> 'Rakefile'
+    echo -n "require 'minitest/unit'\nrequire 'minitest/autorun'\n\nrequire '$project_name'\n" >> 'test/test_helper.rb'
+  cd $curr_dir
+  echo -e  "${clear_color}"
+}
+
